dfe7c7c2649ca22b7dbdef05aa331378c57d9271
EricChen <305044632@qq.com>
2023-10-02 23:00:12 -0700

    Add class World and simple Driver

---
+++ src/world/Driver.java
@@ -0,0 +1,18 @@
+package world;
+
+public class Driver {
+
+  public static void main(String[] args) {
+    World myWorld = World.getWorldInstance();
+    World myWorld2 = World.getWorldInstance();
+    if(args.length < 1) {
+      return;
+    }
+    myWorld.setupWorld(args[0]);
+
+    
+    System.out.println(String.format("Welcome to the world of %s", myWorld.getWorldName()));
+    System.out.println(myWorld == myWorld2);
+  }
+
+}
