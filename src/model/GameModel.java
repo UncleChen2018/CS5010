2b06fdab85d9e7eec6ce428feddcc0d5b440334c
EricChen <305044632@qq.com>
2023-10-19 11:59:07 -0700

    construct the model interface

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -1,5 +1,11 @@
 package model;
 
+/**
+ * The game model interface. Including setup a brand new world, (including
+ * rooms, target, character, player) and all methods that are used to get the
+ * game process, as well as reveal details of the current states.
+ * 
+ */
 public interface GameModel {
-
+  void setupNewWorld(Readable source);
 }
